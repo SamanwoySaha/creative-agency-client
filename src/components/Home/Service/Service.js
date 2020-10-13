5ec83e5e531c1adb8b3ae91930055e032f67996a
SamanwoySaha <samanwoy.kuet@gmail.com>
2020-10-13 15:08:45 +0600

    added services section

--- src/components/Home/Service/Service.js
+++ src/components/Home/Service/Service.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Service.css';
+import { useSpring, animated } from 'react-spring';
 
-const Service = () => {
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
+const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+
+const Service = ({ service }) => {
+    const { pic, title, description } = service;
+    const [state, toggle] = useState(true)
+    const { x } = useSpring({ from: { x: 0 }, x: state ? 1 : 0, config: { duration: 1000 } })
+    
     return (
-        <div>
-            <h1>this is service</h1>
-        </div>
+        <animated.div
+            onClick={() => toggle(!state)}
+            style={{
+                opacity: x.interpolate({ range: [0, 1], output: [0.3, 1] }),
+                transform: x
+                    .interpolate({
+                        range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+                        output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1]
+                    })
+                    .interpolate(x => `scale(${x})`)
+            }} 
+            className="service text-center col-md-4 mt-4">
+            <div className="mx-5 py-5">
+                <img className="service-img img-fluid mb-4" src={pic} alt="" />
+                <h5 className="service-title mb-2">{title}</h5>
+                <p className="service-description">{description}</p>
+            </div>
+        </animated.div>
     );
 };
 
--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import Service from '../Service/Service';
 import './Services.css';
 
 const Services = () => {
+    const services = [
+        {pic: 'https://i.imgur.com/1nOuv1K.png', title: 'Web & Mobile Design', description: 'We craft stunning and amazing web UI, using a well drrafted UX to fit your product.'},
+        {pic: 'https://i.imgur.com/1nOuv1K.png', title: 'Web & Mobile Design', description: 'We craft stunning and amazing web UI, using a well drrafted UX to fit your product.'},
+        {pic: 'https://i.imgur.com/1nOuv1K.png', title: 'Web & Mobile Design', description: 'We craft stunning and amazing web UI, using a well drrafted UX to fit your product.'},
+    ];
     return (
-        <div>
-            <h1>this is services</h1>
-            <Service></Service>
-        </div>
+        <Container className="services px-0">
+            <h1 className="services-heading">Providing awesome <span className="special">services</span></h1>
+            <div className="row mt-5">
+                {
+                    services.map(service => <Service service={service}></Service>)
+                }
+            </div>
+        </Container>
     );
 };
 
