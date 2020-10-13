0787e7ab4abe8925252b224800f2d5ea43b08c89
SamanwoySaha <samanwoy.kuet@gmail.com>
2020-10-13 11:49:10 +0600

    added banner

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 import './Banner.css';
 
 const Banner = () => {
     return (
-        <div>
-            <h1>this is banner</h1>
+        <div className="banner">
+            <Container className="px-0">
+                <div className="row align-items-center">
+                    <div className="col-sm-12 col-md-5">
+                        <h1 className="banner-heading mb-4">Let’s Grow Your Brand To The Next Level</h1>
+                        <p className="sec-color banner-article mb-4">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus commodo ipsum duis laoreet maecenas. Feugiat
+                            </p>
+                        <button className="main-btn">Hire us</button>
+                    </div>
+                    <div className="col-sm-12 col-md-7">
+                        <img src="https://i.imgur.com/xviGCWB.png" alt="" className="img-fluid" />
+                    </div>
+                </div>
+            </Container>
         </div>
     );
 };
